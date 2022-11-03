7664d49c207fe6d12fbcdf7cf3d1d765b300c60b
SYoneya <102698630+SYoneya@users.noreply.github.com>
2022-11-03 13:22:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ TOKEN = '5449016317:AAHly8zYgBWSUVKY3vEQvpYvTOMsIlAmAdQ'
 
 async def on_startup(_):
     print('Bot was started!')
+    await bot.send_message(-1001591876770, f'''Бот обновлён и запущен.''')
