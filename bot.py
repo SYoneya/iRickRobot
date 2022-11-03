fe415f5bea4ce5870b037ea088e0812f48be248c
SYoneya <102698630+SYoneya@users.noreply.github.com>
2022-11-03 22:17:57 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -293,7 +293,7 @@ async def ban_cmd(message: types.Message):
         elif not message.reply_to_message:
             await message.reply(f'''Нужно в ответ на сообщение.''')
             return
-        elif member.status not in {'member'}:
+        elif member.status in {'administrator', 'creator'}:
             await message.reply(f'''Ты не можешь дать бан администратору.''')
             return
         ban_time = int(message.text.split()[1])
