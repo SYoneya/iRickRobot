629a76a091a64d90f7d7701992d12ea58a55ccc4
SYoneya <102698630+SYoneya@users.noreply.github.com>
2022-11-05 20:32:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -340,7 +340,7 @@ async def unmute_cmd(message: types.Message):
 @dp.message_handler(commands=['разбан', 'unban'], commands_prefix='/!.')
 async def unban_cmd(message: types.Message):
     member = await bot.get_chat_member(message.chat.id, message.from_user.id)
-    if member.status in {'member'}
+    if member.status in {'member'}:
         await message.reply(f'''Ты не можешь дать разбан, так как не имеешь прав администратора.''')
         return
     elif not message.reply_to_message:
