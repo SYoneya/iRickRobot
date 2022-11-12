7f37f7338d514379267498c0850cc313c752d162
SYoneya <yoneyadev@gmail.com>
2022-11-12 22:23:16 +0200

    irickrobot

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys; from aiogram import Bot, Dispatcher; from config import *;
 bot = Bot(token=TOKEN, parse_mode='HTML')
 dp = Dispatcher(bot)
 
-sys.path.insert(0, 'C:\Users\yoney\Documents\Projects\Python\iRickRobot\admin_tools')
+sys.path.insert(1, 'C:\Users\yoney\Documents\Projects\Python\iRickRobot\admin_tools')
 
 async def on_startup(_):
     await bot.send_message(-1001591876770, f'''Бот обновлен и запущен!''')
