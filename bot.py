03f54ba29ae0cfeafbfd6d0461e2019c1aa844e6
SYoneya <yoneyadev@gmail.com>
2022-11-13 20:15:50 +0200

    irickrobot

--- bot.py
+++ bot.py
@@ -2,6 +2,10 @@ from imports import *;
 
 
 
+@dp.message_handler(lambda message: message.text.casefold() == 'рик' or message.text.casefold() == 'бот')
+async def ping_cmd(message: types.Message):
+    await message.reply('✅ На месте!', reply_markup=kb1)
+    
 @dp.message_handler(commands=['start'], commands_prefix='/')
 async def start_cmd(message: types.Message):
     await message.reply('Привет!', reply_markup=kb1)
--- main.py
+++ main.py
@@ -9,7 +9,3 @@ logging.basicConfig(level=logging.INFO)
 
 async def on_startup(_):
     await bot.send_message(-1001591876770, f'''Бот обновлен и запущен.''')
-
-@dp.message_handler(lambda message: message.text.casefold() == 'рик' or message.text.casefold() == 'бот')
-async def ping_cmd(message: types.Message):
-    await message.reply('✅ На месте!', reply_markup=kb1)
