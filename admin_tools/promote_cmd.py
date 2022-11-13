0585f0ba06fc739803d1f60d5f230412bf46176b
SYoneya <yoneyadev@gmail.com>
2022-11-13 14:00:23 +0200

    irickrobot

--- admin_tools/promote_cmd.py
+++ admin_tools/promote_cmd.py
@@ -6,12 +6,52 @@ from aiogram import types; from aiogram.dispatcher.filters import Command; from 
 async def promote_cmd(message: types.Message, command: Command):
    member = await bot.get_chat_member(message.chat.id, message.from_user.id)
    if member.status not in {'administrator', 'creator'}:
-      await message.reply(f'''Ты не можешь повысить, так как не имеешь прав администратора.''')
+      await message.reply(f'''Ты не можешь дать повышение, так как не имеешь прав администратора.''')
       return
    elif not message.reply_to_message:
       await message.reply(f'''Нужно в ответ на сообщение.''')
       return
-   elif command.args.lower() == '7':
+   admin = await bot.get_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+   if admin.status in {'administrator', 'creator'}:
+      await message.reply(f'''Ты не можешь дать повышение администратору.''')
+      return
+   elif command.args == '0':
+      await bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id, is_anonymous=True)
+      await message.reply(f'''✅ анонимность''')
+      return
+   elif command.args == '1':
+      await bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id, can_manage_chat=True)
+      await message.reply(f'''✅ управление чатом''')
+      return
+   elif command.args == '2':
+      await bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id, can_change_info=True)
+      await message.reply(f'''✅ изменение профиля группы''')
+      return
+   elif command.args == '3':
+      await bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id, can_delete_messages=True)
+      await message.reply(f'''✅ удаление сообщений''')
+      return
+   elif command.args == '4':
+      await bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id, can_restrict_members=True)
+      await message.reply(f'''✅ блокировка пользователей''')
+      return
+   elif command.args == '5':
+      await bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id, can_invite_users=True)
+      await message.reply(f'''✅ добавление участников''')
+      return
+   elif command.args == '6':
+      await bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id, can_pin_messages=True)
+      await message.reply(f'''✅ закрепление сообщений''')
+      return
+   elif command.args == '7':
+      await bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id, can_manage_video_chats=True)
+      await message.reply(f'''✅ управление видеочатами''')
+      return
+   elif command.args == '8':
+      await bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id, can_promote_members=True)
+      await message.reply(f'''✅ выбор администраторов''')
+      return
+   elif command.args == '9':
       await bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id, can_change_info=True, can_delete_messages=True, can_restrict_members=True, can_invite_users=True, can_pin_messages=True, can_manage_video_chats=True, can_promote_members=True)
       await message.reply(f'''✅ измение профиля группы
 ✅ удаление сообщений
@@ -22,5 +62,14 @@ async def promote_cmd(message: types.Message, command: Command):
 ✅ выбор администраторов
 ❌ анонимность''')
       return
-   elif command.args.lower() == '1':
-      await bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id, can_manage_chat=True)
+   elif command.args == '10':
+      await bot.promote_chat_member(message.chat.id, message.reply_to_message.from_user.id, can_change_info=True, can_delete_messages=True, can_restrict_members=True, can_invite_users=True, can_pin_messages=True, can_manage_video_chats=True, can_promote_members=True, is_anonymous=True)
+      await message.reply(f'''✅ измение профиля группы
+✅ удаление сообщений
+✅ блокировка пользователей
+✅ добавление участников
+✅ закрепление сообщений
+✅ управление видеочатами
+✅ выбор администраторов
+✅ анонимность''')
+      return
