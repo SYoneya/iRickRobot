cf3a0c6c95eece4fe6bb09394c5082dff249314d
SYoneya <102698630+SYoneya@users.noreply.github.com>
2022-11-30 19:26:48 +0200

    Update memes.py

--- games/memes.py
+++ games/memes.py
@@ -8,4 +8,4 @@ async def rickroll(message: types.Message):
 
 @dp.message_handler(lambda message: message.text.casefold() == 'мем')
 async def mem(message: types.Message):
-   await bot.copy_message(message.chat.id, -1001882577870, random.randint(4, 39), protect_content=True, reply_to_message_id=message.message_id)
+   await bot.copy_message(message.chat.id, -1001882577870, random.randint(4, 57), protect_content=True, reply_to_message_id=message.message_id)
