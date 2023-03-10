1b8181b9711a40cb03f1cf55ce2b061fc7bf2b27
SYoneya <yoneyadev@gmail.com>
2023-03-10 22:27:52 +0200

    irickrobot

--- bot.py
+++ bot.py
@@ -4,11 +4,11 @@ from imports import *;
 
 @dp.message_handler(lambda message: message.text.casefold() == 'рик' or message.text.casefold() == 'бот')
 async def ping_cmd(message: types.Message):
-    await message.reply('✅ На месте!', reply_markup=kb1)
+    await message.reply('✅ На месте!')
     
 @dp.message_handler(commands=['start'], commands_prefix='/')
 async def start_cmd(message: types.Message):
-    await message.reply('Привет!', reply_markup=kb1)
+    await message.reply('Привет!')
 
 
 
